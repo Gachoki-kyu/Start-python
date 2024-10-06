e17ebfb100da29aea18e503c60993349ce649eee
Gachoki-kyu <gachoki.14044@students.kyu.ac.ke>
2024-10-06 22:54:21 +0300

    done

---
+++ casting.py
@@ -0,0 +1,27 @@
+#int
+
+x = int(2)
+y = int(4.3)
+z = int("5")
+print(x)
+print(y)
+print(z)
+
+#float
+
+i = float(3)
+j = float(7.56)
+k = float("56.53")
+l = float("7")
+print(i)
+print(j)
+print(z)
+print(k)
+
+#string
+q = str(3)
+s = str("65")
+t = str(6.7)
+print(q)
+print(s)
+print(t)
