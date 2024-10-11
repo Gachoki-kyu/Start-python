5b01db9560e0aefa0bfc4b2d8fcef460104fe8b4
Gachoki-kyu <gachoki.14044@students.kyu.ac.ke>
2024-10-11 21:39:23 +0300

    just uploaded for loop

---
+++ for.py
@@ -0,0 +1,19 @@
+# if..elif...else
+
+x = 50
+y = 10
+if x <= y:
+    print("x is less than 10")
+elif x == 20:
+    print("x is equal to 20")
+else:
+    print("x is greater than 10")
+
+    #for
+a = ["kenya","mozambique","uganda",'tazania',"sudan"]
+for i in a:
+    print(i) #prints the list
+for t in "banana":
+    print(t)  #print the string banana in each character
+for w in range(0,5):
+ print(w) #prints numbers from 0 to 4
