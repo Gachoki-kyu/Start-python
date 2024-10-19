6425a6fd9bd0bc1dc195c1ec530888de5166223e
Gachoki-kyu <gachoki.14044@students.kyu.ac.ke>
2024-10-19 14:14:59 +0300

    file handling

---
+++ file.py
@@ -0,0 +1,22 @@
+# file handling in python
+f = open("demo.txt", "r")
+#print(f.read())  #reads the file
+print(f.readline()) # reads the first line of the file
+print(f.readline()) # reads the second line
+f.close()
+
+# wrtting in file
+x = open("demo.txt", 'a')
+x.write("added some new content to the file ")
+x.close()
+#open the file
+x = open("demo.txt", "r")
+print(x.read())
+x.close()
+
+#deleting a file
+import os
+try:
+    os.remove("demo.txt")
+except:
+    print("file does not exist")
