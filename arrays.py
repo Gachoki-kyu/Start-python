0aca6bd23580c0ee852b38992e1a5441ade5fbde
Gachoki-kyu <gachoki.14044@students.kyu.ac.ke>
2024-10-24 00:04:13 +0300

    mid

--- arrays.py
+++ arrays.py
@@ -1,11 +1,24 @@
 import numpy as np
+from numpy import random
+'''
 arr = np.array([1,2,3,4,5])
 print(arr)
 print(type(arr))
+'''
+
 
 # 2-D 
 arafu = np.array([[1,2,3],[4,5,6]])
 print(arafu)
+'''
 print(arafu.ndim)
 print("second element in first row", arafu[0,1])
+for x in arafu:
+   # print(x)
+    for y in x:
+        print(y)
+'''
+x = random.randint(50, size=(3,5))
+print(x)
+
 
--- matlab.py
+++ matlab.py
@@ -1,5 +1,9 @@
+import matplotlib.pyplot as pyp
+import seaborn as sns
 
-
+sns.distplot([0,1,2,3,4,5])
+pyp.show()
+'''
 # Create a TextBlob object
 text = "TextBlob is a great library for processing textual data."
 blob = TextBlob(text)
@@ -9,3 +13,4 @@ print("Original text:", blob)
 print("Word count:", len(blob.words))
 print("Sentence count:", len(blob.sentences))
 print("Sentiment:", blob.sentiment)
+'''
